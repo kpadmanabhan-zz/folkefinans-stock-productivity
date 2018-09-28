8da561c3418bffacb7705eba4e6a69d5cb214102
kpadmanabhan <krispv@gmail.com>
2018-09-28 13:23:53 +0200

    initial commit with all files

---
+++ Folkefinans.StockProductivity/Folkefinans.StockProductivity/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Folkefinans.StockProductivity
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void btnEnterStockDetails_Click(object sender, EventArgs e)
+        {
+            if (!HttpContext.Current.User.Identity.IsAuthenticated) {
+                Response.Redirect(@"Account\Login.aspx");
+            }
+        }
+
+        protected void btnViewCalculatedResult_Click(object sender, EventArgs e)
+        {
+            if (!HttpContext.Current.User.Identity.IsAuthenticated) {
+                Response.Redirect(@"Account\Login.aspx");
+            }
+        }
+    }
+}
